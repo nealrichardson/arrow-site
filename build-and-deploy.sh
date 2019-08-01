4fc03d5a09d141a52f89ed40f19c489db6a27c4b
Neal Richardson <neal.p.richardson@gmail.com>
2019-08-01 09:36:31 -0700

    Build script refactoring to eliminate need for STAGING_URL

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -2,26 +2,31 @@
 set -ev
 
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]; then
+    # Set git config so that the author of the deployed site commit is the same
+    # as the author of the commit we're building
     export AUTHOR_EMAIL=$(git log -1 --pretty=format:%ae)
     export AUTHOR_NAME=$(git log -1 --pretty=format:%an)
     git config --global user.email "${AUTHOR_EMAIL}"
     git config --global user.name "${AUTHOR_NAME}"
 
-    export STAGING_URL="https://"$(echo $TRAVIS_REPO_SLUG | sed 's@/@.github.io/@')
-    if [ ! -z "${STAGING_URL}" ]; then
-        perl -pe 's@^baseurl.*@baseurl: '"${STAGING_URL}"'@' -i _config.yml
-        export TARGET_BRANCH=gh-pages
-    elif [ "${TRAVIS_REPO_SLUG}" = "apache/arrow-site" ]; then
+    if [ "${TRAVIS_REPO_SLUG}" = "apache/arrow-site" ]; then
         # Production
         export TARGET_BRANCH=asf-site
-        perl -pe 's@^baseurl.*@baseurl: https://arrow.apache.org@' -i _config.yml
+        export BASE_URL=https://arrow.apache.org
     else
-        echo "You must set a STAGING_URL environment variable in the Travis repository settings"
-        exit 1
+        # On a fork, so we'll deploy to GitHub Pages
+        export TARGET_BRANCH=gh-pages
+        # You could supply an alternate BASE_URL, but that's not necessary
+        # because we can infer it based on GitHub Pages conventions
+        if [ -z "${BASE_URL}" ]; then
+            export BASE_URL="https://"$(echo $TRAVIS_REPO_SLUG | sed 's@/@.github.io/@')
+        fi
     fi
+    # Set the site.baseurl
+    perl -pe 's@^baseurl.*@baseurl: '"${BASE_URL}"'@' -i _config.yml
 
     if [ -z "${GITHUB_PAT}" ]; then
-        echo "You must set a GITHUB_PAT in the Travis repository settings"
+        echo "To publish the site, you must set a GITHUB_PAT in the Travis repository settings"
         exit 1
     fi
 
@@ -34,7 +39,6 @@ if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]; 
     git clone -b ${TARGET_BRANCH} https://${GITHUB_PAT}@github.com/$TRAVIS_REPO_SLUG.git OUTPUT
     rsync -r build/ OUTPUT/
     cd OUTPUT
-
 
     git add .
     git commit -m "Updating built site (build ${TRAVIS_BUILD_NUMBER})" || true
